62c1ceeb9b057d9d7ee7ca5a0d86084e96bf5a7a
4tlc <tcroniniv@gmail.com>
2022-09-21 10:04:20 -0400

    fix start in tmux func

--- zshrc/alias.sh
+++ zshrc/alias.sh
@@ -2,6 +2,7 @@ alias vi='nvim'
 alias siftop='sudo iftop -i en0'
 alias c="clear"
 alias sexit="tmux kill-server"
+alias dt="tmux detach"
 
 alias galgo='cd ~/Documents/School/Algorithms/'
 alias algotext='open ~/Documents/School/Algorithms/AlgorithmsTextbook.pdf'
--- zshrc/start.sh
+++ zshrc/start.sh
@@ -1,3 +1,10 @@
 # boot into tmux
 # if [ "$TMUX" = "" ]; then tmux; fi
+# if [ -z "$TMUX" ]; then
+#   tmux attach || tmux
+#fi
+if [ -z "$TMUX" ]
+then
+    tmux attach -t TMUX || tmux new -s TMUX
+fi
 
