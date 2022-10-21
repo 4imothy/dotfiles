9fb813aea267f7cf8e3475dc17c45b2471d55775
4tlc <tcroniniv@gmail.com>
2022-10-21 15:50:15 -0400

    update function for red errors

--- zshrc/loaded_scripts/errorcolors.zsh
+++ zshrc/loaded_scripts/errorcolors.zsh
@@ -1,10 +1,11 @@
+export COLOR_RED="$(tput setaf 1)"
+export COLOR_RESET="$(tput sgr0)"
+
 exec 9>&2
 exec 8> >(
-    while IFS='' read -r line || [ -n "$line" ]; do
-       echo -e "\033[31m${line}\033[0m"
-    done
+    perl -e '$|=1; while(sysread STDIN,$a,9999) {print "$ENV{COLOR_RED}$a$ENV{COLOR_RESET}"}'
 )
-function undirect(){ exec 2>&9; }
-function redirect(){ exec 2>&8; }
+function undirect(){ exec 2>&9; } # reset to original 9 (==2)
+function redirect(){ exec 2>&8; } # set to custom 8
 trap "redirect;" DEBUG
 PROMPT_COMMAND='undirect;'
