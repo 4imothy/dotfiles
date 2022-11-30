6ade65b92ee5e5d8d025b8e6da61a0e0fc1895ac
Timothy Cronin <tcroniniv@gmail.com>
2022-11-29 20:11:02 -0500

    update alias

--- zshrc/loaded_scripts/alias.sh
+++ zshrc/loaded_scripts/alias.sh
@@ -22,26 +22,3 @@ alias la='ls -a'
 alias reload="source ~/.zshrc"
 
 alias -s {js,ts,html,java,sh,md,py}="nvim"
-
-# go to locations
-alias algor='cd ~/Documents/School/Algorithms/'
-alias algotext='open ~/Documents/School/Algorithms/AlgorithmsTextbook.pdf'
-
-alias linea='cd ~/Documents/School/Linear'
-alias linetext='open ~/Documents/School/Linear/LinearTextbook.pdf'
-
-alias logic='cd ~/Documents/School/LogicDesign'
-alias logitext='open ~/Documents/School/LogicDesign/LogicTextbook.pdf';
-
-alias micro='cd ~/Documents/School/Micro/'
-alias calcu='cd ~/Documents/School/Calc3/'
-alias gnote='cd ~/Documents/Vault/🏫\ SchoolNotes'
-
-# open common apps
-alias obs='open /Applications/Obsidian.app'
-alias fir='open /Applications/Firefox.app'
-alias spo='open /Applications/Spotify.app'
-alias mai='open /System/Applications/Mail.app'
-alias mes='open /System/Applications/Messages.app'
-alias cal='open /System/Applications/Calendar.app'
-alias saf='open /Applications/Safari.app/'
