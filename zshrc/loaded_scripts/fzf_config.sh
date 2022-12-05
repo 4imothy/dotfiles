0320bf74d41bde066d3f79f674b2340405bdb86b
Timothy Cronin <tcroniniv@gmail.com>
2022-12-05 12:55:28 -0500

    update readme

--- zshrc/loaded_scripts/fzf_config.sh
+++ zshrc/loaded_scripts/fzf_config.sh
@@ -3,20 +3,12 @@ export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 export FZF_DEFAULT_COMMAND="fd --type f"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND";
 
-f() {
-    sels=( "${(@f)$(fd "${fd_default[@]}" "${@:2}"| fzf)}" )
-    test -n "$sels" && print -z -- "$1 ${sels[@]:q:q}"
-}
-
-# Like f, but not recursive.
-fm() f "$@" --max-depth 1
-
-fd() {
+find_dir() {
   DIR=`find * -maxdepth 0 -type d -print 2> /dev/null | fzf-tmux` \
     && cd "$DIR"
 }
 # fda - including hidden directories
-fda() {
+find_dir_a() {
   local dir
   dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
 }
