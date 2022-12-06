20686192fb39826ab37cf5855954113ffe31b27b
Timothy Cronin <tcroniniv@gmail.com>
2022-12-06 12:19:12 -0500

    update fzf functions

--- zshrc/loaded_scripts/fzf_config.sh
+++ zshrc/loaded_scripts/fzf_config.sh
@@ -12,3 +12,14 @@ find_dir_a() {
   local dir
   dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
 }
+
+# navigate through the current directory
+nd(){
+    cd $( ls | fzf)
+}
+
+search_for_dir(){
+    cd $( find . \( -path '*/Library/*' -prune -o -path '*/node_modules/*' -prune -o -path '*/Pictures/*' -prune -o -path '*/.*' -prune \) -o type -d | fzf )
+    # dir = `find . \( -path '*/Library/*' -prune -o -path '*/node_modules/*' -prune -o -path '*/Pictures/*' -prune -o -path '*/.*' -prune \) -o type -d | fzf ` \
+    # && cd "$dir"
+}
