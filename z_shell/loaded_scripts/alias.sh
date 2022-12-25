e1cb8486c97faf363a44a9ac2dbf2970102f98f1
Timothy Cronin <tcroniniv@gmail.com>
2022-12-25 16:06:47 -0600

    change folder name

---
+++ z_shell/loaded_scripts/alias.sh
@@ -0,0 +1,28 @@
+alias vi='nvim'
+alias siftop='sudo iftop -i en0'
+alias c="clear"
+alias ch="history -p; clear"
+alias ct='clear; tmux clear-history; clear' # tmux clear
+
+# git
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+
+alias py='python3'
+alias pip='pip3'
+
+# tmux
+alias switch="tmux switch -t"
+
+alias l='ls'
+alias ll='ls -l'
+alias la='ls -a'
+
+alias reload="source ~/.zshrc"
+
+alias -s {js,ts,html,java,sh,md,py}="nvim"
+
+# other functions
+alias amtp="add_math_scripts_to_path"
---
+++ z_shell/loaded_scripts/git_functions.sh
@@ -0,0 +1,31 @@
+gpushthistomain() {
+    git add .
+    git commit -m $1
+    git push -u origin main
+}
+
+gpushdotfiles() {
+    cp -r ~/loaded_scripts ~/Projects/seperate_git_repos/dotfiles/zshrc/
+    cp ~/.zshrc ~/Projects/seperate_git_repos/dotfiles/zshrc
+    cp ~/.tmux.conf ~/Projects/seperate_git_repos/dotfiles/zshrc
+    cp ~/.hushlogin ~/Projects/seperate_git_repos/dotfiles/zshrc
+    cp -Rf ~/.config/nvim ~/Projects/seperate_git_repos/dotfiles
+    cp -Rf ~/Documents/Vault/.obsidian/snippets/ ~/Projects/seperate_git_repos/dotfiles/obsidian
+
+    git -C ~/Projects/seperate_git_repos/dotfiles add .
+    git -C ~/Projects/seperate_git_repos/dotfiles commit -m $1
+    git -C ~/Projects/seperate_git_repos/dotfiles push -u origin main
+}
+
+gpushknowledgebase() {
+    git -C ~/Documents/Vault/🏫\ SchoolNotes add .
+    git -C ~/Documents/Vault/🏫\ SchoolNotes commit -m $1
+    git -C ~/Documents/Vault/🏫\ SchoolNotes  push -u origin main
+}
+
+gpushmushroomdriverscripts() {
+    cp -r ~/Projects/Mushroom\ Driver/Mushroom\ Driver/Assets/Scripts ~/Projects/SeperateGitRepos/Mushroom\ Driver
+    git -C ~/Projects/SeperateGitRepos/Mushroom\ Driver add .
+    git -C ~/Projects/SeperateGitRepos/Mushroom\ Driver commit -m $1
+    git -C ~/Projects/SeperateGitRepos/Mushroom\ Driver push -u origin main
+}
