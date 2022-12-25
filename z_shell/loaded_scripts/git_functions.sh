ab3c7581ffcd5fed2237cde751c22b37142ec396
Timothy Cronin <tcroniniv@gmail.com>
2022-12-25 16:12:51 -0600

    update dotfiles push function for new folder name

--- z_shell/loaded_scripts/git_functions.sh
+++ z_shell/loaded_scripts/git_functions.sh
@@ -5,10 +5,10 @@ gpushthistomain() {
 }
 
 gpushdotfiles() {
-    cp -r ~/loaded_scripts ~/Projects/seperate_git_repos/dotfiles/zshrc/
-    cp ~/.zshrc ~/Projects/seperate_git_repos/dotfiles/zshrc
-    cp ~/.tmux.conf ~/Projects/seperate_git_repos/dotfiles/zshrc
-    cp ~/.hushlogin ~/Projects/seperate_git_repos/dotfiles/zshrc
+    cp -r ~/loaded_scripts ~/Projects/seperate_git_repos/dotfiles/z_shell/
+    cp ~/.zshrc ~/Projects/seperate_git_repos/dotfiles/z_shell
+    cp ~/.tmux.conf ~/Projects/seperate_git_repos/dotfiles/z_shell
+    cp ~/.hushlogin ~/Projects/seperate_git_repos/dotfiles/z_shell
     cp -Rf ~/.config/nvim ~/Projects/seperate_git_repos/dotfiles
     cp -Rf ~/Documents/Vault/.obsidian/snippets/ ~/Projects/seperate_git_repos/dotfiles/obsidian
 
--- zshrc/basic.sh
+++
@@ -1,5 +0,0 @@
-export EDITOR='nvim'
-bindkey -v
-bindkey "^?" backward-delete-char
-setopt autocd
-unsetopt BEEP
