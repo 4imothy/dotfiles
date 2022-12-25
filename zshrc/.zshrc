1740129bf95f89ec3397393656e8ea8e4a13ab5e
Timothy Cronin <tcroniniv@gmail.com>
2022-12-25 16:15:19 -0600

    update path to images

--- zshrc/.zshrc
+++
@@ -1,9 +0,0 @@
-for FILE in ~/loaded_scripts/*; do
-    source $FILE
-done
-
-# boot into tmux
-if [ -z "$TMUX" ]
-    then
-	 tmux attach -t def || tmux new -s def
-fi
--- zshrc/path_config.zsh
+++
@@ -1,7 +0,0 @@
-local FZF_PATH=~/.fzf
-if [[ ! "$PATH" == *${FZF_PATH}/bin* ]]; then
-  export PATH="$PATH:${FZF_PATH}/bin"
-fi
-if [[ ! "$PATH" == *$HOME/bin* ]]; then
-  export PATH="$PATH:$HOME/bin"
-fi
