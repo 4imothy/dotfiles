eda933a61570eaf229e33f4adb698694fa4351f0
Timothy Cronin <tcroniniv@gmail.com>
2023-01-05 23:33:00 -0500

    better comments

--- z_shell/loaded_scripts/path_config.zsh
+++ z_shell/loaded_scripts/path_config.zsh
@@ -1,6 +1,8 @@
+# for scripts
 if [[ ! "$PATH" == *$HOME/bin* ]]; then
   export PATH="$PATH:$HOME/bin"
 fi
+# for fzf on command line
 if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
   export PATH="$PATH:$HOME/.fzf/bin"
 fi
