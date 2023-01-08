1ea523d07971066e8bcd2e5c22e52775bf377ffb
Timothy Cronin <tcroniniv@gmail.com>
2023-01-08 12:01:38 -0500

    update locations

--- qutebrowser/binds.py
+++
@@ -1,8 +0,0 @@
-# open in new tab
-config.bind('t', 'set-cmd-text -s :open -t')
-# hide statusbar
-config.bind('xb', 'config-cycle statusbar.show always never')
-# hide tabs
-config.bind('xt', 'config-cycle tabs.show always never')
-# hide both bar and tabs
-config.bind('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
