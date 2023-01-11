ef964b6eb9207dad711fafb0284a2be11c411d2a
Timothy Cronin <tcroniniv@gmail.com>
2023-01-11 11:12:59 -0500

    add rust config

--- z_shell/loaded_scripts/path_config.zsh
+++ z_shell/loaded_scripts/path_config.zsh
@@ -1,8 +1,20 @@
-# for scripts
+# for scripts i made
 if [[ ! "$PATH" == *$HOME/bin* ]]; then
   export PATH="$PATH:$HOME/bin"
 fi
+
 # for qmake
 if [[ ! "$PATH" == */opt/homebrew/opt/qt5/bin* ]]; then
   export PATH="$PATH:/opt/homebrew/opt/qt5/bin"
 fi
+
+# to initialize for rust development
+start_rust() { 
+  if [[ ! "$PATH" == *$HOME/.cargo/bin* ]]; then
+    source $HOME/.cargo/env
+  fi 
+  if [[ ! "$PATH" == *$HOME/.rustup/toolchains/stable-aarch64-apple-darwin/bin* ]]; then
+    export PATH="$HOME/.rustup/toolchains/stable-aarch64-apple-darwin/bin:$PATH"
+  fi
+}
+
