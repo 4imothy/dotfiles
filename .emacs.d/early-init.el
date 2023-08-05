5cddfdc5206bb3f70c3d265fbd0ccac29187a179
Timothy Cronin <tcroniniv@gmail.com>
2023-08-05 17:33:57 -0400

    better garbage collection on startup

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,1 +1,6 @@
+(defvar my/gc-cons-threshold gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold my/gc-cons-threshold)))
 (setq vc-follow-symlinks t)
