5595754539e68b7f403bb4f8c7f0da78527f7d4d
Timothy Cronin <tcroniniv@gmail.com>
2023-12-11 16:17:59 -0500

    better colors

--- .zsh/completion.sh
+++ .zsh/completion.sh
@@ -3,10 +3,9 @@ compinit -d ~/.cache/zsh/zcompdump-$ZSH_VERSION
 zstyle ':completion:*' completer _extensions _complete _approximate
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' menu yes select
-zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
 zstyle ':completion:*:default' list-colors ${LS_COLORS}
-zstyle ':completion:*:*:*:*:descriptions' format '%F{135}-- %d --%f'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{magenta}-- %d --%f'
 zstyle '*' single-ignored complete
 setopt ALWAYS_TO_END
 
